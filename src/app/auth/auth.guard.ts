60132fe721d36f6274703e3feb2394f82f1b4f8d
GUMILLANF <gumillanf@gmail.com>
2023-08-17 22:49:01 -0300

    start

---
+++ src/app/auth/auth.guard.ts
@@ -0,0 +1,17 @@
+import { ActivatedRouteSnapshot, CanActivateFn, Router, RouterStateSnapshot } from '@angular/router';
+import { AuthService } from '../services/auth.service';
+import { inject } from '@angular/core';
+
+export const authGuard: CanActivateFn = (route: ActivatedRouteSnapshot, state: RouterStateSnapshot) => {
+  const authService = inject(AuthService);
+  const router = inject(Router);
+
+  let authenticated = authService.isAuthenticated();
+
+  if (authenticated) {
+    return true;
+  } else {
+    router.navigate(['login']);
+    return false;
+  }
+};
